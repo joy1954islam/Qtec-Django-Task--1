53eadce0cbae8686e6f63292942feb6a81bcbbe5
joyislam1954 <joyislam1954@gmail.com>
2021-04-24 18:59:24 +0600

    fixed bugs

--- Qtec_Django/settings.py
+++ Qtec_Django/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Qtec_Django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'Qtec_Django.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
+        'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 

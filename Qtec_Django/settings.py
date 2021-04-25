7c7e686944d8d3213be1695a5c95fa4cba70be02
joyislam1954 <joyislam1954@gmail.com>
2021-04-25 21:23:26 +0600

    again change settings files

--- Qtec_Django/settings.py
+++ Qtec_Django/settings.py
@@ -32,12 +32,12 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'courier.apps.CourierConfig',
-    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
 
     'crispy_forms',
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'StaticFiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
